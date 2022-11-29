15919882c337d8edb90e9ce47554c494f537bfac
Totolow <67413794+Totolow@users.noreply.github.com>
2022-11-29 16:34:30 +0100

    Initial commit

---
+++ excelEinlesen.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+person1_tabelle = pd.read_excel("C:/Users/Timon/Downloads/Person1BAN.xlsx")
+person2_tabelle = pd.read_excel("C:/Users/Timon/Downloads/Person2BAN.xlsx")
+
+print(person1_tabelle)
+print(person2_tabelle)
